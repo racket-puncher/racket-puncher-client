0d8dc9bffb46f9eacdfaf3c28a4fd0007bbf9539
songyi <jrl103@naver.com>
2023-10-21 19:30:49 +0900

    [Feature] axios interceptor setting #15

---
+++ lib/http/httpInterceptor.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const http = axios.create({
+	baseURL: '',
+	timeout: 10000,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	withCredentials: true,
+});
+
+http.interceptors.request.use(
+	(config) => {
+		return config;
+	},
+	(ignore) => {}
+);
+
+http.interceptors.response.use(
+	(res) => {
+		return res;
+	},
+	(ignore) => {}
+);
+
+export default http;
