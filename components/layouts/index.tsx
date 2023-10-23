3f4731c79124555451d106452b329f73bca5aeb1
songyi <jrl103@naver.com>
2023-10-23 21:53:21 +0900

    [Feature] font-family 추가/적용, 로그인페이지 생성, 공통 레이아웃 잡기 #36

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from 'react';
 import LoadingSpin from '../common/loading/spin';
 import { loadingState } from '../../lib/store/common/index';
 import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import { rem } from 'polished';
 
 interface ILayoutProps {
 	children: ReactNode;
@@ -11,8 +13,16 @@ export default function LayoutContainer(props: ILayoutProps) {
 	const isShow = useRecoilValue(loadingState);
 	return (
 		<>
-			<div>{props.children}</div>
-			{isShow && <LoadingSpin />}
+			<LayoutWrapper>
+				<div>{props.children}</div>
+				{isShow && <LoadingSpin />}
+			</LayoutWrapper>
 		</>
 	);
 }
+
+const LayoutWrapper = styled.div`
+	min-height: 100vh;
+	max-width: ${rem('640px')};
+	margin: auto;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import * as B from '../styles/ts/common';
-import { rem } from 'polished';
 
 export default function MainPage() {
-	return (
-		<>
-			<Test>ddd</Test>
-		</>
-	);
+	return <></>;
 }
-
-const Test = styled.p`
-	color: ${B.MainColor};
-	font-size: ${rem('18px')};
-`;
