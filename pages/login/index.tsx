c66b2e436a109b05052ea50bab28c3664e7ef66a
songyi <jrl103@naver.com>
2023-10-24 22:08:46 +0900

    [Feature] useToast hook create #48

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { PageMainTitle } from '../../styles/ts/components/titles';
+import useToast from '../../utils/useToast';
 
 export default function Login() {
+	const { success, contextHolder } = useToast();
 	return (
 		<>
+			{contextHolder}
 			<PageMainTitle>로그인</PageMainTitle>
+			<button onClick={() => success('성공입니다.')}></button>
 		</>
 	);
 }
