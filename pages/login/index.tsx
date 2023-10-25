0273b03b080fd89863e5c989ee532aeebeca9753
syji <jrl10346@gmail.com>
2023-10-25 16:54:18 +0900

    [Feature] 공통 input css 컴포넌트 생성 #50

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { PageMainTitle } from '../../styles/ts/components/titles';
-import useToast from '../../utils/useToast';
+import { InputBox } from '../../components/common/input';
+import styled from 'styled-components';
 
 export default function Login() {
-	const { success, contextHolder } = useToast();
+	// const { success, contextHolder } = useToast();
 	return (
 		<>
-			{contextHolder}
 			<PageMainTitle>로그인</PageMainTitle>
-			<button onClick={() => success('성공입니다.')}></button>
+			<InputContainer>
+				<InputBox>
+					<label htmlFor='loginEmail'>이메일</label>
+					<input id='loginEmail' />
+				</InputBox>
+				<InputBox>
+					<label htmlFor='loginPwd'>비밀번호</label>
+					<input id='loginPwd' />
+				</InputBox>
+			</InputContainer>
 		</>
 	);
 }
+
+const InputContainer = styled.div`
+	margin-top: 50px;
+`;
