6fb9ba13db63c39531addcb2619c71a51b8057ed
JS0O <floresoota@gamil.com>
2023-10-27 09:01:53 +0900

    feat(elements): Create pages - my, post

---
+++ pages/my/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styled from 'styled-components';
+// import { rem } from 'polished';
+
+const MyPage = () => {
+	return (
+		<>
+			<h2>마이페이지</h2>
+		</>
+	);
+};
+
+export default MyPage;
