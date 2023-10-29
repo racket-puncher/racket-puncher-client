7b3c282627d1c9761e2bf2ed8462afc4717ff869
songyi <jrl103@naver.com>
2023-10-29 21:29:00 +0900

    [Feature] react-slick 적용 #122

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Slider from 'react-slick';
 import { rem } from 'polished';
+import type { RadioChangeEvent, TabsProps } from 'antd';
+import { Tabs } from 'antd';
 
 import { ImageBox } from '../styles/ts/components/box';
 
 const settings = {
-	dots: true,
+	arrows: false,
+	dots: false,
 	infinite: true,
 	speed: 500,
 	// autoplay: true,
@@ -15,6 +18,19 @@ const settings = {
 };
 
 export default function MainPage() {
+	const items: TabsProps['items'] = [
+		{
+			key: 'matchingList',
+			label: '매칭리스트',
+			children: '매칭리스트입니다',
+		},
+		{
+			key: 'myAroundMatching',
+			label: '내 주변 매칭',
+			children: '내 주변 매칭입니다',
+		},
+	];
+
 	return (
 		<>
 			<MainViewContainer>
@@ -29,18 +45,59 @@ export default function MainPage() {
 					</Slider>
 				</SliderContainer>
 
-				<div className='matchList'></div>
+				<MainContainer>
+					<CustomTab defaultActiveKey='1' items={items} />
+				</MainContainer>
 			</MainViewContainer>
 		</>
 	);
 }
 
-const MainViewContainer = styled.div`
-	margin-top: 20px;
-`;
+const MainViewContainer = styled.div``;
 
 const SliderContainer = styled.div`
 	div.slick-slide {
 		width: ${rem('640px')};
+	}
+`;
+
+const MainContainer = styled.div`
+	margin-top: 30px;
+`;
+
+const CustomTab = styled(Tabs)`
+	width: 100% !important;
+	font-size: inherit !important;
+	line-height: 1 !important;
+	color: inherit !important;
+	div.ant-tabs-nav {
+		margin: 0 !important;
+		&::before {
+			display: none !important;
+		}
+		div.ant-tabs-nav-wrap {
+			display: block !important;
+			&::before {
+				display: none !important;
+			}
+			&::after {
+				display: none !important;
+			}
+			div.ant-tabs-nav-list {
+				display: block !important;
+				div.ant-tabs-tab {
+					display: block !important;
+					padding: 0 !important;
+					font-size: inherit !important;
+					margin: 0 !important;
+				}
+				div.ant-tabs-ink-bar {
+					display: none !important;
+				}
+			}
+		}
+		div.ant-tabs-nav-operations {
+			display: none !important;
+		}
 	}
 `;
