a936c4d2ad74897d1fdf08835368e42ba6f5fb9f
songyi <jrl103@naver.com>
2023-10-29 21:48:59 +0900

    [Feature] 레이아웃 조건부 설정 #126

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -1,21 +1,38 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import LoadingSpin from '../common/loading/spin';
 import styled from 'styled-components';
 import { rem } from 'polished';
 
 import Header from './Header';
+import { useRouter } from 'next/router';
 
 interface ILayoutProps {
 	readonly children: ReactNode;
 }
 
+interface ILayoutAlignProps {
+	readonly isPadding: boolean;
+}
+
 export default function LayoutContainer(props: ILayoutProps) {
+	const router = useRouter();
+	const [isPadding, setIsPadding] = useState(true);
+
+	useEffect(() => {
+		if (router.pathname === '/') {
+			console.log('진입!!');
+			setIsPadding(false);
+		} else {
+			setIsPadding(true);
+		}
+	}, [router.pathname]);
+
 	return (
 		<>
 			<LoadingSpin>
 				<LayoutWrapper>
 					<Header />
-					<LayoutAlign>{props.children}</LayoutAlign>
+					<LayoutAlign isPadding={isPadding}>{props.children}</LayoutAlign>
 				</LayoutWrapper>
 			</LoadingSpin>
 		</>
@@ -28,9 +45,11 @@ const LayoutWrapper = styled.div`
 	margin: auto;
 `;
 
-const LayoutAlign = styled.div`
+const LayoutAlign = styled.div.withConfig({
+	shouldForwardProp: (props) => props !== 'isPadding',
+})<ILayoutAlignProps>`
 	min-height: 100vh;
 	display: flex;
 	flex-direction: column;
-	padding: 0 ${rem('30px')};
+	padding: ${(props) => (props.isPadding ? `${rem('0px')} ${rem('30px')}` : 'initial')};
 `;
