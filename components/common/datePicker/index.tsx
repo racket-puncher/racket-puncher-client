bc9d335e4d0351c60383966e7077109276755ddc
songyi <jrl103@naver.com>
2023-11-02 00:57:19 +0900

    [Feature] react-moblie-datepicker 적용 #161

---
+++ components/common/datePicker/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState, Dispatch, SetStateAction } from 'react';
+import DatePicker from 'react-mobile-datepicker';
+import styled from 'styled-components';
+import { ImageBox } from '../../../styles/ts/components/box';
+import { FontSizeSpSm } from '../../../styles/ts/common';
+import { rem } from 'polished';
+import { dateFomatter } from '../../../utils/fomatter';
+
+interface IDatePickerProps {
+	readonly dateState: Date;
+	readonly setDateState: Dispatch<SetStateAction<Date>>;
+}
+
+const options = {
+	headerFormat: 'YYYY년 MM월 DD일',
+	dateFormat: ['YYYY', 'M', 'D'],
+	locale: 'ko',
+	confirmText: '확인',
+	cancelText: '취소',
+};
+
+export default function CustomDatePicker(props: IDatePickerProps) {
+	const [isOpen, setIsOpen] = useState(false);
+	const [showDateStr, setShowDateStr] = useState('');
+
+	const handleSelect = (selectedDate: Date) => {
+		props.setDateState(selectedDate);
+		setShowDateStr(dateFomatter(selectedDate));
+		setIsOpen(false);
+	};
+	return (
+		<>
+			<CustomDatePickerBox onClick={() => setIsOpen(true)}>
+				<p>{showDateStr}</p>
+				<ImageBox width={'24px'} height={'24px'}>
+					<img src='/images/calendar.png' alt='calendar' />
+				</ImageBox>
+			</CustomDatePickerBox>
+			<DatePicker
+				isOpen={isOpen}
+				onSelect={handleSelect}
+				onCancel={() => setIsOpen(false)}
+				date={props.dateState}
+				{...options}
+			/>
+		</>
+	);
+}
+
+const CustomDatePickerBox = styled.div`
+	width: 100%;
+	padding: 17px ${rem('14px')};
+	border-radius: 10px;
+	border: 1px solid #dcdcdc;
+	background: #f9f9f9;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	cursor: pointer;
+	p {
+		font-size: ${rem(`${FontSizeSpSm}`)};
+		font-family: Pretendard-Regular;
+	}
+`;
