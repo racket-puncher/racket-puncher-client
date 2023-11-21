387d95cc3f06aaeb42a8e24258f222e727071cfe
JS0O <floresoota@gamil.com>
2023-11-22 05:49:23 +0900

    feat (usersService): Create and Edit api functions

--- service/users/interface.ts
+++ service/users/interface.ts
@@ -1,2 +1,27 @@
 // 매칭 리스트 조회
-export interface reqGetUserInfoApiData {}
+export interface reqGetUserInfoApiData {
+	userId: string;
+}
+
+export interface reqPostUserProfileImg {
+	URL: string;
+}
+
+export interface reqPatchMyProfileInfo {
+	nickname: string; // 닉네임
+	password: string; // 비밀번호
+	email: string; // 이메일
+	phoneNumber: string; // 전화번호
+	address: string; // 주소
+	zipCode: string; // 우편번호
+	ntrp: string; // NTRP
+	gender: string; // 성별
+	ageGroup: string; // 연령대
+}
+
+export interface reqPostReportUser {
+	title: string; // 제목
+	content: string; // 내용
+	writerId: string; // 신고자 ID
+	reportedId: string; // 피신고자 ID
+}
