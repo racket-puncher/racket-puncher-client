7a1a8024a377002bd9f598801863f4aa3a17ffde
JS0O <floresoota@gamil.com>
2023-11-22 08:50:10 +0900

    refactor (myPage)

--- pages/my/index.tsx
+++ pages/my/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-// import { rem } from 'polished';
+import { rem } from 'polished';
 import { RoundButton } from '../../styles/ts/components/buttons';
 import { TabsProps } from 'antd';
 import { CustomTab } from '../../styles/ts/components/tab';
@@ -10,51 +10,37 @@ import MyMatchingList from '../../components/contents/my/myMatchingList';
 import useCookies from '../../utils/useCookies';
 import useToast from '../../utils/useToast';
 import { useRouter } from 'next/router';
+import useRouterHook from 'utils/useRouterHook';
 
-const MyPage = () => {
-	const { checkLogin } = useCookies();
+const items: TabsProps['items'] = [
+	{
+		key: 'hostedMatchingList',
+		label: '등록한 매칭',
+		children: <MyMatchingList listType='hosted' />,
+	},
+	{
+		key: 'appliedMatchingList',
+		label: '신청한 매칭',
+		children: <MyMatchingList listType='applied' />,
+	},
+];
+
+const MyPage = (userId: string) => {
+	// 프로필 수정 페이지 라우팅
+
+	const { checkLogin, getCookie } = useCookies();
 	const { setMessage } = useToast();
-	const router = useRouter();
-
-	// To-do
-	// userInfos get 요청
-
-	const userInfos = {
-		userNickName: '왕자',
-		age: 30,
-		gender: '남',
-		userAddress: '서울시 성동구',
-		ntrp: 'Pro',
-		winningRate: [1, 3],
-		mannerPoint: 85,
-		realName: '김개발',
-		email: 'princeofracket@gmail.com',
-		imageURL: '',
-	};
-
-	const items: TabsProps['items'] = [
-		{
-			key: 'submittedMatchingList',
-			label: '등록한 매칭',
-			children: <MyMatchingList listType='submitted' />,
-		},
-		{
-			key: 'appliedMatchingList',
-			label: '신청한 매칭',
-			children: <MyMatchingList listType='applied' />,
-		},
-	];
-
-	useEffect(() => {
-		if (!checkLogin()) {
-			setMessage('error', '로그인이 필요한 서비스입니다.');
-			router.replace('/login');
-		}
-	}, []);
+	const { replace } = useRouterHook();
+	// useEffect(() => {
+	// 	if (!checkLogin()) {
+	// 		setMessage('error', '로그인이 필요한 서비스입니다.');
+	// 		replace('/login');
+	// 	}
+	// }, []);
 
 	return (
 		<>
-			<MyProfile userInfos={userInfos} />
+			<MyProfile userId={'5'} />
 			<RoundButton colorstyle='is-black' aria-label='프로필 수정페이지로 이동'>
 				프로필 수정
 			</RoundButton>
@@ -68,5 +54,5 @@ const MyPage = () => {
 export default MyPage;
 
 const ListArea = styled.div`
-	margin-top: 30px;
+	margin-top: ${rem('30px')};
 `;
